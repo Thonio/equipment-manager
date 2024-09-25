7c579348f818d4b6a7f9ddd4eabbb7785102a9a8
Thonio <anthonio.rj@hotmail.fr>
2024-09-25 19:47:22 +0300

    configuration route & css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { route } from './routes/route.tsx'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './thenme.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={route} />
+    </ThemeProvider>
   </StrictMode>,
 )
